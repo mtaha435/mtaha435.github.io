529601ff42285ef975d4301d820b279a0d664f8a
Mohamed Taha <168931405+mtaha435@users.noreply.github.com>
2025-04-13 20:08:19 -0400

    added index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+//greeting fade in/out
+const greetings = ["Hello", "مرحبا", "Hola", "Bonjour", "Ciao"]
+let i = 0;
+const textElement = document.getElementById("hello_text");
+function animate_greeting(){
+    textElement.style.opacity = 0;
+    setTimeout(()=>{
+        i = (i + 1) % greetings.length;
+        if(i==1){
+            textElement.style.fontSize="29px";
+        }
+        else{
+            textElement.style.fontSize="40px";
+        }
+        textElement.textContent = greetings[i];
+        textElement.style.opacity = 1;
+    }, 1000);
+
+}
+textElement.style.opacity=1;
+setInterval(animate_greeting,3000);
